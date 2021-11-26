9b6867a52edfba2f16420662a175ca5d099791b1
Andrew121410 <andrewcherring@gmail.com>
2021-11-26 00:33:05 -0500

    Customize name of proxy

--- src/main/java/com/andrew121410/mc/doubleabungeecord/DoubleANetwork.java
+++ src/main/java/com/andrew121410/mc/doubleabungeecord/DoubleANetwork.java
@@ -3,6 +3,11 @@ package com.andrew121410.mc.doubleabungeecord;
 import com.andrew121410.mc.doubleabungeecord.commands.DiscordCMD;
 import com.andrew121410.mc.doubleabungeecord.commands.HubCMD;
 import com.andrew121410.mc.doubleabungeecord.listeners.OnServerConnectedEvent;
+import com.andrew121410.simpleruntimepatcher.SimpleRuntimePatcher;
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtMethod;
+import javassist.LoaderClassPath;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public class DoubleANetwork extends Plugin {
@@ -12,6 +17,8 @@ public class DoubleANetwork extends Plugin {
     @Override
     public void onEnable() {
         plugin = this;
+
+        patchUp();
         registerListeners();
         registerCommands();
     }
@@ -24,9 +31,35 @@ public class DoubleANetwork extends Plugin {
         new OnServerConnectedEvent(this);
     }
 
-    public void registerCommands() {
+    private void registerCommands() {
         new HubCMD(this);
         new DiscordCMD(this);
+    }
+
+    private void patchUp() {
+        try {
+            Class<?> theClass = Class.forName("net.md_5.bungee.BungeeCord");
+
+            SimpleRuntimePatcher.patch(theClass, (classLoader, bytes) -> {
+                ClassPool classPool = new ClassPool();
+                classPool.appendClassPath(new LoaderClassPath(classLoader));
+                try {
+                    CtClass ctClass = classPool.get(theClass.getName());
+                    CtMethod ctMethod = ctClass.getDeclaredMethod("getName");
+                    ctMethod.insertBefore("if (true) return \"DoubleA\";");
+                    byte[] byteCode = ctClass.toBytecode();
+                    ctClass.detach();
+                    return byteCode;
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return bytes;
+            });
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        SimpleRuntimePatcher.create();
     }
 
     public static DoubleANetwork getInstance() {
